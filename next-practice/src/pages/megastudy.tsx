01a38096020de9ac356d44c5d7d015852e694a48
kseonghee <kseonghee0127@gmail.com>
2024-03-25 16:46:24 +0900

    feat: 폴더구조

---
+++ next-practice/src/pages/megastudy.tsx
@@ -0,0 +1,4 @@
+const Megastudy = () => {
+	return <div>메가스터디 페이지입니다.</div>;
+};
+export default Megastudy;
---
+++ next-practice/src/pages/megastudy/frontEnd.tsx
@@ -0,0 +1,4 @@
+const FrontEnd = () => {
+	return <div>프론트앤드 페이지입니다.</div>;
+};
+export default FrontEnd;
